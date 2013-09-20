2e269a31a6ad0bb8e263c0d9f964db8622e4b084
Lars Nielsen <lars@lfweb.dk>
2013-09-21 01:04:22 +0200

    make some output

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 
 all:
 	$(MAKE) -C kernel
+
+run:
+	qemu-system-x86_64 -kernel kernel/myos.bin -m 32
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -113,5 +113,5 @@ void kernel_main()
 	terminal_initialize();
 	/* Since there is no support for newlines in terminal_putchar yet, \n will
 	   produce some VGA specific character instead. This is normal. */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("Hello, kernel World!");
 }
